b9729d0ca9684ed966d52d4acdca4096f8970725
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-11 03:23:17 +0530

    Test cases are added

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,48 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import android.app.Application;
 import android.test.ApplicationTestCase;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest {
+
+    private static ExpenseManager expenseManager;
+
+    @Before
+    public void setupApp(){
+        expenseManager = new PersistentExpenseManager(ApplicationProvider.getApplicationContext());
     }
+
+    @Test
+    public void addAccountTest(){
+       expenseManager.addAccount("12345","Test Bank","Test User",1000.00);
+       List<String> accountNumbers =  expenseManager.getAccountNumbersList();
+       assertTrue(accountNumbers.contains("12345"));
+    }
+
+    @Test
+    public void checkNotExistingAccountTest(){
+        expenseManager.addAccount("12345","Test Bank","Test User",1000.00);
+        List<String> accountNumbers =  expenseManager.getAccountNumbersList();
+        assertFalse(accountNumbers.contains("11111"));
+    }
+
+
+
 }
